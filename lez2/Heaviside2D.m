de844ebb2cf5a01bd85a6c94c70fd9a419b37096
mate377 <matteo.finazzer@gmail.com>
2017-03-02 18:46:45 +0100

    code commented

--- lez2/Heaviside2D.m
+++ lez2/Heaviside2D.m
@@ -1,3 +1,4 @@
+% The implementation follow exactly the definition and is straightforward
 function [out] = Heaviside2D(x,y)
     if x<0 || y<0
         out=0;
--- lez2/sinexp.m
+++ lez2/sinexp.m
@@ -1,4 +1,9 @@
 clear;
+% Here I use the symbolic toolbox to have a taylor expansion without using
+% a loop. Variable N is the order at which the expansion stop. Going
+% through 10 periods of the sine is really too much, with N=110 still i'm
+% not able to cover the real sine completely. With 3 periods and N=25 the
+% result is ok, showing the polynomial departing near 3
 N=25;
 x=sym('x');
 fx=sin(2*pi*x);
